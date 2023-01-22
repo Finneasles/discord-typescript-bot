fd01e719deeeeb495e1de8feac233b9d72dbac40
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-01-22 22:58:25 +0000

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,25 +1,30 @@
-import { AutocompleteInteraction,CommandInteraction, Message, PermissionResolvable, SlashCommandBuilder } from "discord.js";
+import {
+  AutocompleteInteraction,
+  CommandInteraction,
+  Message,
+  PermissionResolvable,
+  SlashCommandBuilder,
+} from "discord.js";
 
-export interface SlashCommand {
-    command: SlashCommandBuilder | any,
-    execute: (interaction : CommandInteraction) => void,
-    autocomplete?: (interaction: AutocompleteInteraction) => void,
-    cooldown?: number
-}
+// export interface SlashCommand {
+//     command: SlashCommandBuilder | any,
+//     execute: (interaction : CommandInteraction) => void,
+//     autocomplete?: (interaction: AutocompleteInteraction) => void,
+//     cooldown?: number
+// }
 
-export interface Command {
-    parameter: string,
-    execute: (message: Message, args: Array<string>) => void,
-    permissions: Array<PermissionResolvable>,
-    aliases: Array<string>,
-    cooldown?: number,
-}
-
-export interface Event {
-    name: string,
-    execute: (message: Message, args: Array<string>) => void,
+export interface Action {
+  name?: string;
+  description?: string;
+  parameter?: string;
+  execute: (message: Message, args: Array<string>) => void;
+  permissions?: Array<PermissionResolvable>;
+  aliases?: Array<string>;
+  cooldown?: number;
+  slash?: boolean;
+  autocomplete?: (interaction: AutocompleteInteraction) => void;
 }
 
 export interface GuildOption {
-    [key: string]: any,
+  [key: string]: any;
 }
